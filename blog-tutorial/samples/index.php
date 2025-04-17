ccb00acf02ff8a7e69f800b10c3f54c507fa67f4
Fabio Bas <fabio.bas@officineinformatiche.net>
2025-04-17 19:00:04 +0200

    Blog tutorial: fix overview links and samples

---
+++ blog-tutorial/samples/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Blog tutorial samples</title>
+</head>
+<body>
+
+<h1>Blog tutorial samples</h1>
+<ol>
+	<li><a href="day1/blog/">Day 1</a></li>
+	<li><a href="day2/blog/">Day 2</a></li>
+	<li><a href="day3/blog/">Day 3</a></li>
+	<li><a href="day4/blog/">Day 4</a></li>
+	<li><a href="day5/blog/">Day 5</a></li>
+</ol>
+</body>
+</html>
